402743f26a5dc135f7ce76a7c66287a98c9cd049
Marcos-AS <marcossantangelo1@gmail.com>
2025-03-26 09:25:35 -0300

    spring iniciado

---
+++ pga/src/main/java/unlu/sip/pga/PgaApplication.java
@@ -0,0 +1,13 @@
+package unlu.sip.pga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgaApplication.class, args);
+	}
+
+}
