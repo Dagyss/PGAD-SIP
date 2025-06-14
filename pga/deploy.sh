7b5d735141a8027bcaaf82b8c5928f409a819adb
Marcos-AS <54771734+Marcos-AS@users.noreply.github.com>
2025-06-14 06:23:35 -0300

    ingress para desplegar el front y ademas usar https

--- pga/deploy.sh
+++ pga/deploy.sh
@@ -6,3 +6,6 @@ kubectl apply -f deploy/configmaps/
 kubectl apply -f deploy/services/
 kubectl apply -f deploy/statefulset/
 kubectl apply -f deploy/deployment/
+kubectl apply -f deploy/ingress/
+kubectl apply -f deploy/clusterIssuer/
+kubectl apply -f deploy/certificate/
